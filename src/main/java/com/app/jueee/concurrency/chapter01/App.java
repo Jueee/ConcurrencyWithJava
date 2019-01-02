520f00421191ddcd40ba0609765f0e7cab21b1b7
尉勇强2 <hzweiyongqiang@corp.netease.com>
2019-01-02 19:36:37 +0800

    first commit

---
+++ src/main/java/com/app/jueee/concurrency/chapter01/App.java
@@ -0,0 +1,13 @@
+package com.app.jueee.concurrency.chapter01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
