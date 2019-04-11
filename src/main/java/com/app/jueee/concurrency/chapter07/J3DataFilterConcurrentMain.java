5b30d57c49b77a94c252b007ded1b6c019df531f
尉勇强2 <hzweiyongqiang@corp.netease.com>
2019-04-11 19:19:53 +0800

    并发数据筛选算法

---
+++ src/main/java/com/app/jueee/concurrency/chapter07/J3DataFilterConcurrentMain.java
@@ -0,0 +1,192 @@
+package com.app.jueee.concurrency.chapter07;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
+import com.app.jueee.concurrency.chapter07.common.CensusData;
+import com.app.jueee.concurrency.chapter07.common.CensusDataLoader;
+import com.app.jueee.concurrency.chapter07.common.FilterData;
+
+public class J3DataFilterConcurrentMain {
+
+    /**
+     * 用于查找满足筛选器条件的第一个数据对象
+     * 
+     * @param data
+     * @param filters
+     * @return
+     */
+    public static CensusData findAny(CensusData[] data, List<FilterData> filters, int size) {
+        TaskManager manager = new TaskManager();
+        IndividualTask task = new IndividualTask(data, 0, data.length, manager, size, filters);
+        ForkJoinPool.commonPool().execute(task);
+        CensusData result = task.join();
+        if (result != null) {
+            System.out.println("Find Any Result: "+result.getCitizenship());
+            return result;
+        }
+        return null;
+    }
+
+    /**
+     * 用于查找满足筛选器条件的所有数据对象
+     * 
+     * @param data
+     * @param filters
+     * @return
+     */
+    public static List<CensusData> findAll(CensusData[] data, List<FilterData> filters, int size) {
+        TaskManager manager = new TaskManager();
+        ListTask task = new ListTask(data, 0, data.length, manager, size, filters);
+        ForkJoinPool.commonPool().execute(task);
+        List<CensusData> result = task.join();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 从文件加载数据
+        Path path = Paths.get("data", "chapter07//census-income.data");
+        CensusData data[] = CensusDataLoader.load(path);
+        System.out.println("Number of items: " + data.length);
+        Date start, end;
+        final int SIZE;
+        if (args.length > 0) {
+            SIZE = Integer.valueOf(args[0]);
+        } else {
+            SIZE = 4000;
+        }
+        
+        // 一、使用 findAny() 方法查找出现在数组中第一个位置的对象
+        List<FilterData> filters = new ArrayList<>();
+        FilterData filter = new FilterData();
+        filter.setIdField(32);
+        filter.setValue("Dominican-Republic");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(31);
+        filter.setValue("Dominican-Republic");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(1);
+        filter.setValue("Not in universe");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(14);
+        filter.setValue("Not in universe");
+        filters.add(filter);
+        start = new Date();
+        CensusData result = findAny(data, filters, SIZE);
+        System.out.println("Test 1 - Result: " + result.getReasonForUnemployment());
+        end = new Date();
+        System.out.println("Test 1- Execution Time: " + (end.getTime() - start.getTime()));
+
+        // 二、使用 findAny() 方法查找出现在数组中最后一个位置的对象
+        filters = new ArrayList<>();
+        filter = new FilterData();
+        filter.setIdField(32);
+        filter.setValue("United-States");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(31);
+        filter.setValue("Greece");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(1);
+        filter.setValue("Private");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(14);
+        filter.setValue("Not in universe");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(0);
+        filter.setValue("62");
+        filters.add(filter);
+
+        start = new Date();
+        result = findAny(data, filters, SIZE);
+        System.out.println("Test 2 - Result: " + result.getReasonForUnemployment());
+        end = new Date();
+        System.out.println("Test 2- Execution Time: " + (end.getTime() - start.getTime()));
+
+        // 三、使用 findAny() 方法尝试查找某个并不存在的对象
+        filters = new ArrayList<>();
+        filter = new FilterData();
+        filter.setIdField(32);
+        filter.setValue("XXXX");
+        filters.add(filter);
+
+        start = new Date();
+        result = findAny(data, filters, SIZE);
+        if (result == null) {
+            System.out.println("Test 3 - Result: " + result);
+        } else {
+            System.out.println("Test 3 - Result: " + result.getReasonForUnemployment());
+        }
+        end = new Date();
+        System.out.println("Test 3 - Execution Time: " + (end.getTime() - start.getTime()));
+
+        // 四、在错误情境中使用 findAny() 方法
+        filters = new ArrayList<>();
+        filter = new FilterData();
+        filter.setIdField(0);
+        filter.setValue("Dominican-Republic");
+        filters.add(filter);
+
+        start = new Date();
+        try {
+            result = findAny(data, filters, SIZE);
+            System.out.println("Test 4 - Results: " + result.getCitizenship());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        end = new Date();
+        System.out.println("Test 4 - Execution Time: " + (end.getTime() - start.getTime()));
+
+        // 五、使用 findAll() 方法获取满足筛选器列表条件的所有对象
+        filters = new ArrayList<>();
+        filter = new FilterData();
+        filter.setIdField(32);
+        filter.setValue("Dominican-Republic");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(31);
+        filter.setValue("Dominican-Republic");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(1);
+        filter.setValue("Not in universe");
+        filters.add(filter);
+        filter = new FilterData();
+        filter.setIdField(14);
+        filter.setValue("Not in universe");
+        filters.add(filter);
+
+        start = new Date();
+        List<CensusData> results = findAll(data, filters, SIZE);
+        System.out.println("Test 5 - Results: " + results.size());
+        end = new Date();
+        System.out.println("Test 5 - Execution Time: " + (end.getTime() - start.getTime()));
+
+        // 六、在错误情境中使用 findAll() 方法
+        filters = new ArrayList<FilterData>();
+        filter = new FilterData();
+        filter.setIdField(0);
+        filter.setValue("Dominican-Republic");
+        filters.add(filter);
+
+        start = new Date();
+        try {
+            results = findAll(data, filters, SIZE);
+            System.out.println("Test 6 - Results: " + results.size());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        end = new Date();
+        System.out.println("Test 6 - Execution Time: " + (end.getTime() - start.getTime()));
+    }
+}
---
+++ src/main/java/com/app/jueee/concurrency/chapter07/ListTask.java
@@ -0,0 +1,83 @@
+package com.app.jueee.concurrency.chapter07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+import com.app.jueee.concurrency.chapter07.common.CensusData;
+import com.app.jueee.concurrency.chapter07.common.Filter;
+import com.app.jueee.concurrency.chapter07.common.FilterData;
+
+public class ListTask extends RecursiveTask<List<CensusData>> {
+
+ // 含有所有 CensusData 对象的数组
+    private CensusData[] data;
+    // Start 属性和 end 属性，它们确定了要处理的元素
+    private int start, end;
+    // 确定了在无须分割任务的前提下所处理的最大元素数
+    private int size;
+    // 用于在必要之时撤销任务
+    private TaskManager manager;
+    private List<FilterData> filters;
+
+    public ListTask(CensusData data[], int start, int end, TaskManager manager, int size, List<FilterData> filters) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+        this.size = size;
+        this.filters = filters;
+        this.manager = manager;
+    }
+    
+    @Override
+    protected List<CensusData> compute() {
+        List<CensusData> ret = new ArrayList<>();
+        List<CensusData> tmp;
+        
+        // 如果任务要处理的元素数量小于 size 属性，将满足筛选器指定标准的所有对象添加到结果列表中
+        if (end - start <= size) {
+            for (int i = start; i < end; i++) {
+                CensusData censusData = data[i];
+                if (Filter.filter(censusData, filters)) {
+                    ret.add(censusData);
+                }
+            }
+        // 如果要处理的项数多于 size 属性，将创建两个子任务来处理其中各一半的元素。
+        } else {
+            int mid = (start + end) / 2;
+            ListTask task1 = new ListTask(data, start, mid, manager, size, filters);
+            ListTask task2 = new ListTask(data, mid, end, manager, size, filters);
+            // 将新创建的任务添加到任务管理器并且删除原来的实际任务
+            manager.addTask(task1);
+            manager.addTask(task2);
+            manager.deleteTask(this);
+            // 使用 fork() 方法以异步方式将任务发送给 ForkJoinPool ，并且使用 quietlyJoin() 方法等待其执行结束
+            task1.fork();
+            task2.fork();
+            task2.quietlyJoin();
+            task1.quietlyJoin();
+            // 从 TaskManager 中删除子任务
+            manager.deleteTask(task1);
+            manager.deleteTask(task2);
+
+            
+            try {
+                tmp = task1.join();
+                if (tmp != null) {
+                    ret.addAll(tmp);
+                }
+                manager.deleteTask(task1);
+            } catch (Exception e) {}
+            
+            try {
+                tmp = task2.join();
+                if (tmp != null) {
+                    ret.addAll(tmp);
+                }
+                manager.deleteTask(task2);
+            } catch (Exception e) {}
+        }
+        return ret;
+    }
+
+}
